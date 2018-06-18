50bbe8bcacfbf361e027edafbdbb442319d82055
fischteichit@gmx.de <charley>
2018-06-18 20:41:15 +0200

    data folder issue solved (absolute path)

--- belavoco_server/__init__.py
+++ belavoco_server/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-
+import os
 from belavoco_server.app_uploader.controllers import app_uploader
 from belavoco_server.api.controllers import api
 
+
+UPLOAD_FOLDER = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    '..',
+    'data')
 
 app = Flask(__name__,
             instance_relative_config=True,
@@ -11,7 +16,7 @@ app = Flask(__name__,
 
 
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['UPLOAD_FOLDER'] = 'data'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #Maximum Size:
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
 
