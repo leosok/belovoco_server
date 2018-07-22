08cd1263602fce411d637c0cae6df97b0d9e42e3
fischteichit@gmx.de <charley>
2018-07-22 15:52:51 +0200

    added UPLOAD button. No no auto-upload. could even check for title.

--- belavoco_server/app_uploader/templates/belavoco_fileupload.js
+++ belavoco_server/app_uploader/templates/belavoco_fileupload.js
@@ -7,15 +7,31 @@ $(function () {
     var formData;
     console.log(url);
   
+    $('.upload_button').prop('disabled', true);
+
+
 
     $('#fileupload1').fileupload({  
         
         url: url,
         dataType: 'json',
-        change: function (e, data) {
+        autoUpload: false,
+
+        add: function (e, data) {       
+               
+
           $.each(data.files, function (index, file) {
-              //alert('Selected file: ' + file.name);
-              data.formData = $('form').serializeArray();
+
+              console.log('Selected file: ' + file.name);
+              $('.upload_button').prop('disabled', false);
+              
+              //data.formData = $('form').serializeArray();           
+
+              data.context = $('.upload_button')
+                .click(function () {
+                    data.context.text('Uploading...');
+                    data.submit();
+                });
           })
         },  
         drop: function (e, data) {
@@ -25,13 +41,17 @@ $(function () {
             });
         },      
         done: function (e, data) {
+
+            $('.upload_button').text("Done!").prop("disabled",true);
+            $('.filename_text').toggleClass('bg-info bg-success');
+            $('.filename_text').prepend("✔ ");
           //console.log("Upload finished!"); 
           //console.log($('form').serializeArray())
-  
+            /*
             $.each(data.result.files, function (index, file) {
                 $('<p/>').text(file.name).appendTo('#files');
                 alert(file.name + ' erfolgreich hochgeladen!');
-            });
+            }); */
         },
         progressall: function (e, data) {
             var progress = parseInt(data.loaded / data.total * 100, 10);
@@ -40,6 +60,24 @@ $(function () {
                 progress + '%'
             );
         }
-    }).prop('disabled', !$.support.fileInput)
+    })
+    .on('fileuploadadd', function (e, data) {
+        data.context = $('<div/>').appendTo('#files');
+        $.each(data.files, function (index, file) {
+            var node = $('<p/>')
+                    .append($('<span class="filename_text bg-info" style="padding: 5px"/>').text(file.name));
+            if (!index) {
+                node
+                    .append('<br>')
+            }
+            node.appendTo(data.context);
+        });
+    })    
+    .prop('disabled', !$.support.fileInput)
         .parent().addClass($.support.fileInput ? undefined : 'disabled');
   });
+
+
+
+
+  
