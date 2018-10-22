0e9e532740324c344eb3eac330d28c27c7c115c4
fischteichit@gmx.de <charley>
2018-10-22 22:35:50 +0300

    now like master

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from belavoco_server import app
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host= '0.0.0.0')
+    app.run(debug=True,host= '0.0.0.0', port=8080)
     
     #remove_dead_files(app.config['UPLOAD_FOLDER'])
     
