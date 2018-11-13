261ef9c72115576688d97d0421969e50e2c46833
fischteichit@gmx.de <charley>
2018-11-13 23:24:23 +0100

    Auf Standart-Https-Port geändert

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from belavoco_server import app
 if __name__ == '__main__':
 
     context = ('bv_cert.pem', 'bv_key.pem')
-    app.run(host='0.0.0.0', port=8080, ssl_context=context, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=443, ssl_context=context, threaded=True, debug=True)
 
 
     app.config['SEND_PUSH'] = False
