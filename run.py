47818f1a65288fa4c9aa907d8c69e5c65efe368f
fischteichit@gmx.de <charley>
2018-11-13 22:44:05 +0100

    added ssl for local server

--- run.py
+++ run.py
@@ -5,7 +5,12 @@ from belavoco_server import app
 
 
 if __name__ == '__main__':
+
+    context = ('bv_cert.pem', 'bv_key.pem')
+    app.run(host='0.0.0.0', port=8080, ssl_context=context, threaded=True, debug=True)
+
+
     app.config['SEND_PUSH'] = False
-    app.run(debug=True,host= '0.0.0.0', port=8080)
     
-    #remove_dead_files(app.config['UPLOAD_FOLDER'])
+    #Uncomment for HTTP
+    #app.run(debug=True,host= '0.0.0.0', port=8080, ssl_context=context)
