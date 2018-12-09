bfb7dc46c4f6913a99986230ecddd7f6f12bc793
fischteichit@gmx.de <charley>
2018-12-09 13:36:26 +0100

    send no print (encoding shit)

--- belavoco_server/app_uploader/send_pushnotification.py
+++ belavoco_server/app_uploader/send_pushnotification.py
@@ -89,13 +89,13 @@ def push_gun(audiofile):
     if config['SEND_PUSH'] == False:
         print "SEND_PUSH = FALSE | Not sending Message:"
         # Encoding will fix Error on Server, leading to not sending PUSH
-        print message.encode('utf-8')
+        #print message.encode('utf-8')
     
     else:
 
         print "Sending Message:"
         # Encoding will fix Error on Server, leading to not sending PUSH
-        print message.encode('utf-8')
+        #print message.encode('utf-8')
 
         onesignal_client = configure_onesignal()    
         
